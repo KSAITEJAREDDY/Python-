1a2269c1aa3b80362f9d53a2045db92b43c771bd
KSAITEJAREDDY <44354769+KSAITEJAREDDY@users.noreply.github.com>
2025-07-31 17:53:08 +0100

    Add files via upload

---
+++ Products.py
@@ -0,0 +1,45 @@
+import json
+
+class Products:
+    # def __init__(self, name, price, gst, hasgst):
+    #     self.name = name
+    #     self.price = price
+    #     self.gst = gst
+    #     self.hasgst = hasgst
+
+    def jsoninput(self, file = "GST Assignment/products.json"):
+        f = open(file)
+        data = json.load(f)
+        f.close()
+        return data
+
+
+    def getAllGstProducts(self):
+        finallist = []
+        data = self.jsoninput()
+        for v in data.values():
+            for product in v:
+                str1 = str(product["hasgst"])
+                if str1.lower() == 'true':
+                    finallist.append(product)
+        return finallist
+
+
+    def getTotalPrice(self, val=[]):
+        total = 0
+        for item in val:
+            total += item['price']
+        total = total * 1.18
+        return total
+
+
+    def listAllProducts(self):
+        data = self.jsoninput()
+        for v in data.values():
+            for product in v:
+                print("{} : {}".format(product["item number"], product["name"]))
+
+
+
+
+
